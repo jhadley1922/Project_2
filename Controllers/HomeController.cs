e86f337692659b18cb0859856b26bc05b8ebefc4
Sierra Griffin <sierra.griffin14@gmail.com>
2022-03-22 16:13:49 -0600

    commit form

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace Project_2.Controllers
             return View();
         }
 
+        public IActionResult Form()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
